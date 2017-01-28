87c5839604d1a0899853fd6cd36f2da4789ab31f
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2017-01-28 23:17:58 +0200

    initial

---
+++ app/src/main/java/com/example/ahmed/udpsender/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.ahmed.udpsender;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Regex for validating IP addresses
+    private static final Pattern IP_ADDRESS
+            = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+                    + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+                    + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                    + "|[1-9][0-9]|[0-9]))");
+
+    private DatagramSocket socket;
+    private EditText ipEditText;
+    private EditText portEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        // Get the handles of the text fields to use them in validation later
+        // NOTE: this must exist after "setContentView(R.layout.activity_main);"
+        ipEditText = ((EditText) findViewById(R.id.editTextIp));
+        portEditText = ((EditText) findViewById(R.id.editTextPort));
+
+        try {
+            socket = new DatagramSocket();  // Create a UDP socket
+            socket.setBroadcast(true);  // Enable broadcasts
+        } catch (SocketException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        String ipString = ipEditText.getText().toString();
+        String portString = portEditText.getText().toString();
+
+        boolean ipValidated = validateIp(ipString);
+        boolean portValidated = validatePort(portString);
+
+        if (!ipValidated && !portValidated) {
+            showToast("Ip and Port are invalid");
+        } else if (!ipValidated) {
+            showToast("IP is invalid");
+        } else if (!portValidated) {
+            showToast("Port number is invalid");
+        } else {
+            // Correct params
+            Editable message = ((EditText) findViewById(R.id.textAreaMessage)).getText();
+
+            if (message.length() == 0) return;
+
+            byte[] bytes = message.toString().getBytes();
+
+            try {
+
+                // Create the packet containing the message, IP and port number
+                final DatagramPacket packet = new DatagramPacket(bytes, bytes.length,
+                        InetAddress.getByName(ipString), Short.parseShort(portString));
+
+
+                // Network operations must be started on a separate
+                // thread other than the UI thread
+                new Thread() {
+                    public void run() {
+                        try {
+                            // If you want to manipulate the GUI, you must run that
+                            // code on the MainUI thread
+                            socket.send(packet);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+                message.clear();
+            } catch (IOException e) {
+                showToast(e.getMessage());
+            }
+
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean validateIp(String ip) {
+
+
+        if (ip.length() == 0) return false;
+
+        if (!IP_ADDRESS.matcher(ip).matches()) {
+            // Show a message
+            Toast.makeText(getApplicationContext(),
+                    "Please enter a valid IP Address", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validatePort(String text) {
+
+        if (text.length() == 0) return false;
+
+        try {
+            Short.parseShort(text);
+            return true;
+        } catch (NumberFormatException e) {
+            showToast("A valid port number is between 1 and " + Short.MAX_VALUE);
+            return false;
+        }
+
+    }
+}
